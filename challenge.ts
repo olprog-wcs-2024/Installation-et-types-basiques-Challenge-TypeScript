d4a8c49fd83eeb2b549b5e3993f09d211a66445a
gaouimounir <mounir.gaoui@outlook.com>
2024-05-23 17:02:30 +0200

    feat: :art: encapsulation

--- challenge.ts
+++ challenge.ts
@@ -50,4 +50,35 @@ const prettyPrintWilder = (users : Users[]) : string[] => {
   person1.tellMyAge();
   person2.tellMyName();
   person2.tellMyAge();
-  
+
+
+
+  // POO en TS 2 - Encapsulation
+
+  import assert from "assert";
+
+  class BankCustomer {
+    private name: string;
+    private code: string;
+    constructor(name: string, code: string) {
+      this.name = name;
+      this.code = code;
+    }
+
+    getName() {
+      return this.name;
+    }
+
+    verifyPinInput(code: string) {
+      assert.equal(code, this.code);
+    }
+
+
+  }
+
+
+  const customer = new BankCustomer('John Doe', '3579');
+assert.equal(typeof customer.getName, 'function');
+assert.equal(typeof customer.verifyPinInput, 'function');
+assert.equal(customer.getName(), 'John Doe');
+assert.ok(customer.verifyPinInput('3579'));
