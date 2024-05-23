2c9af94814016c30cbd2468573d2c7bf38784749
gaouimounir <mounir.gaoui@outlook.com>
2024-05-23 16:27:45 +0200

    feat: :art: POO en Typescript 1 - Procédural vs Objet, classes, instances

--- challenge.ts
+++ challenge.ts
@@ -22,3 +22,32 @@ const prettyPrintWilder = (users : Users[]) : string[] => {
   prettyPrintWilder(wilders);
 
   console.log(prettyPrintWilder(wilders))
+
+
+  // POO en Typescript 1 - Procédural vs Objet, classes, instances
+
+  class Person{
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+
+    tellMyName() {
+      console.log(`My name is ${this.name}`);
+    }
+
+    tellMyAge() {
+      console.log(`I'm ${this.age} years old`);
+    }
+  }
+
+  const person1 = new Person("John", 40);
+  const person2 = new Person("Mary", 35);
+
+  person1.tellMyName();
+  person1.tellMyAge();
+  person2.tellMyName();
+  person2.tellMyAge();
+  
